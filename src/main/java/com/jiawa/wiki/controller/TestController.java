36d3aac33e2ae4f7e266557ddf13853bc9912e8b
DESKTOP-135JJAB\EdisonHo <1163494766@qq.com>
2022-05-17 23:21:14 +0800

    2-8 add the configuration of own springboot

--- src/main/java/com/jiawa/wiki/controller/TestController.java
+++ src/main/java/com/jiawa/wiki/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.jiawa.wiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,9 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
+    @Value("${test.hello:TEST}")
+    private String testHello;
 
     @GetMapping("/hello")
     public String Hello(){
-        return "hello";
+        return "hello" + testHello;
     }
 }
